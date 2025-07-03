45af425ae9f82b39950ad88b16b273d7a83f848f
lohithpuvvala <lohithpuvvala@gmail.com>
2025-07-03 11:51:50 +0530

    Add DBConnection class to load MySQL config and establish JDBC connection

---
+++ src/com/lohithpuvvala/employeedb/DBConnection.java
@@ -0,0 +1,36 @@
+package com.lohithpuvvala.employeedb;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if(connection != null) {
+            return connection;
+        }
+
+        try{
+            Properties props = new Properties();
+            FileInputStream fis = new FileInputStream("config.properties");
+            props.load(fis);
+
+            String dbUrl = props.getProperty("db.url");
+            String dbUser = props.getProperty("db.user");
+            String dbPassword = props.getProperty("db.password");
+
+            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
